a883b0caf76d2445cd36d508f3a78808d14746cb
LUIZ CLAUDIO MEDINA JUNIOR <luizclaudiomedinajunior@iMac-de-LUIZ-2.local>
2018-06-19 20:35:18 -0300

    Falta forms React Fundamentals

---
+++ Cursos/ASPNET/ApiRestaurante/Model/Migrations/20180514005440_Menu.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Model.Migrations
+{
+    public partial class Menu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Menus",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Apelido = table.Column<string>(nullable: true),
+                    DataCadastro = table.Column<DateTime>(nullable: false),
+                    DataFim = table.Column<DateTime>(nullable: false),
+                    DataInicio = table.Column<DateTime>(nullable: false),
+                    Status = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menus", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MenuRestaurantes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    DataCadastro = table.Column<DateTime>(nullable: false),
+                    MenuId = table.Column<Guid>(nullable: true),
+                    RestauranteId = table.Column<Guid>(nullable: true),
+                    Status = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MenuRestaurantes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MenuRestaurantes_Menus_MenuId",
+                        column: x => x.MenuId,
+                        principalTable: "Menus",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_MenuRestaurantes_Restaurantes_RestauranteId",
+                        column: x => x.RestauranteId,
+                        principalTable: "Restaurantes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuRestaurantes_MenuId",
+                table: "MenuRestaurantes",
+                column: "MenuId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuRestaurantes_RestauranteId",
+                table: "MenuRestaurantes",
+                column: "RestauranteId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MenuRestaurantes");
+
+            migrationBuilder.DropTable(
+                name: "Menus");
+        }
+    }
+}
---
+++ Cursos/ASPNET/ApiRestaurante/Model/Migrations/20180514005828_NotNull.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Model.Migrations
+{
+    public partial class NotNull : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MenuRestaurantes_Menus_MenuId",
+                table: "MenuRestaurantes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_MenuRestaurantes_Restaurantes_RestauranteId",
+                table: "MenuRestaurantes");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "RestauranteId",
+                table: "MenuRestaurantes",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "MenuId",
+                table: "MenuRestaurantes",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MenuRestaurantes_Menus_MenuId",
+                table: "MenuRestaurantes",
+                column: "MenuId",
+                principalTable: "Menus",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MenuRestaurantes_Restaurantes_RestauranteId",
+                table: "MenuRestaurantes",
+                column: "RestauranteId",
+                principalTable: "Restaurantes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MenuRestaurantes_Menus_MenuId",
+                table: "MenuRestaurantes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_MenuRestaurantes_Restaurantes_RestauranteId",
+                table: "MenuRestaurantes");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "RestauranteId",
+                table: "MenuRestaurantes",
+                nullable: true,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "MenuId",
+                table: "MenuRestaurantes",
+                nullable: true,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MenuRestaurantes_Menus_MenuId",
+                table: "MenuRestaurantes",
+                column: "MenuId",
+                principalTable: "Menus",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MenuRestaurantes_Restaurantes_RestauranteId",
+                table: "MenuRestaurantes",
+                column: "RestauranteId",
+                principalTable: "Restaurantes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
